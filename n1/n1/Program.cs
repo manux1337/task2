4081a108776c66a3f9a0ab3697d399f417ed41bf
manux1337 <tagwriter@mail.ru>
2014-09-14 01:29:48 +0600

    task2

---
+++ n1/n1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace n1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.Title = "Task2-1";
+            Console.Write("Введите число в степени в формате {x^k} = ");
+            string _text = Console.ReadLine();
+            UltraPow up = new UltraPow(_text);
+            if (up.success)
+            {
+                Console.WriteLine("\nsqrt({0}) = {1}", _text, up.getPow());
+            }
+            else
+            {
+                Console.WriteLine("\nОшибка!\nДанные введены не верно.");
+            }
+            
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+
+   class UltraPow
+    {
+        private double number;
+        public bool success;
+        public UltraPow(string _pow)
+        {
+            string ptrn = @"([0-9]+)\^([0-9]+)";
+            Match _match = Regex.Match(_pow, ptrn);
+            if (_match.Success)
+            {
+                float n = Convert.ToSingle(_match.Groups[2].Value);
+                float x = Convert.ToSingle(_match.Groups[1].Value);
+                this.number = Math.Pow(x,n);
+                this.success = true;
+            }
+            else
+            {
+                this.success = false;
+            }
+        }
+
+        public double getPow()
+        {
+            double acc = 0.000001;
+            double a = number / 2, b = a + 1;
+            while (Math.Abs(a - b) > acc)
+            {
+                b = a;
+                a = 0.5 * (a + number / a);
+            }
+            return a;
+        }
+    }
+    
+}
---
+++ n2/n2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections;
+
+namespace n2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Task2-2";
+            GO1:Console.Write("Введите положительное число = ");
+            Binary _bin = new Binary();
+            string s = Console.ReadLine();
+            try
+            {
+                _bin.Encode(Convert.ToUInt16(s));
+            }
+            catch (OverflowException e)
+            {
+                if (e.Source != null)
+                    Console.WriteLine("\nОшибка!\nЧисло не положительное.\n");
+                goto GO1;
+            }
+            Console.WriteLine("\n{0} в двоичном коде {{{1}}}", s, _bin.result);
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+
+        }
+
+    }
+     class Binary
+    {
+        private Stack st = new Stack();
+        private string _result;
+        public string result
+        {
+            get
+            {
+                return this._result;
+            }
+        }
+        public void Encode(uint a)
+        {
+            while (a > 0)
+            {
+                this.st.Push((char)a % 2);
+                a /= 2;
+            }
+
+            while (st.Count != 0)
+            {
+                this._result += this.st.Pop();
+            }
+        }
+
+    }
+}
